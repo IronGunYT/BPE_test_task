01c23c62022b28612f13c6fb65562f5f0c12daf6
IronGun <strannik.kraya575721@gmail.com>
2023-04-09 17:51:55 +0300

    feat: add task 4

--- task_4.py
+++ task_4.py
@@ -1,1 +1,30 @@
+import json
+import datetime
 
+
+def replace_fields(d):
+    """
+    Modify dictionary that represents JSON file
+    """
+    for key, value in d.items():
+        if key == 'updated':
+            d[key] = datetime.datetime.now().isoformat()
+        elif isinstance(value, dict):
+            replace_fields(value)
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    replace_fields(item)
+
+
+def correct_json(json_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    replace_fields(data)
+    with open(json_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+if __name__ == '__main__':
+    correct_json('file_4.json')
+    print('JSON file was corrected')
