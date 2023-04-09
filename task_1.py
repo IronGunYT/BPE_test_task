26fdcf564adcf2fa3b8692a136ee4cde025608e7
IronGun <strannik.kraya575721@gmail.com>
2023-04-09 16:46:40 +0300

    feat: add task 1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,13 @@
+def swap_bytes(n: int) -> int:
+    # n to binary
+    s = bin(n)[2:]
+    if len(s) > 8:
+        raise Exception('Bad input')
+    s = '0'*(8-len(s)) + s  # if n has less than 8 bits
+    s = s[4:]+s[:4]  # swap bytes
+    return int(s, 2)
 
+
+if __name__ == '__main__':
+    n = int(input('Enter positive 2-byte integer: '))
+    print('Result:', swap_bytes(n))
